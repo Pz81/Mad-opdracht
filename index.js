00fc809abf297895e2e3662a408eeb7ff90175e5
Pz81 <113420015+Pz81@users.noreply.github.com>
2023-06-25 13:26:30 +0200

    Mad-opdracht

--- index.js
+++ index.js
@@ -5,5 +5,3 @@ const PORT = process.env.PORT || 80;
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
 const server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
-
-
